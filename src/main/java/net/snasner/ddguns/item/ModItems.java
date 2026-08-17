1430697b22e8524c8365e4ad61f27bb05a1f2858
snasner <www.baeroxus@gmail.com>
2026-08-17 15:15:53 -0400

    erm um uh hi

---
+++ src/main/java/net/snasner/ddguns/item/ModItems.java
@@ -0,0 +1,31 @@
+package net.snasner.ddguns.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.snasner.ddguns.dungeonsdelightguns;
+import top.ribs.scguns.init.ModSounds;
+import top.ribs.scguns.item.animated.AnimatedGunItem;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, dungeonsdelightguns.MODID);
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<AnimatedGunItem> GNASHER = ITEMS.register("gnasher",
+            () -> new AnimatedGunItem(
+                    new Item.Properties().stacksTo(1).durability(300),
+                    "gnasher",
+                    ModSounds.MAG_OUT.get(),
+                    ModSounds.MAG_IN.get(),
+                    ModSounds.RELOAD_END.get(),
+                    ModSounds.COPPER_GUN_JAM.get(),
+                    ModSounds.COPPER_GUN_JAM.get()
+            ));
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
